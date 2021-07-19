a0d3d225f44d94ca832a4d1fb6a2d7e6a00fab49
Evan Pavone <evanrpavone@gmail.com>
2021-07-19 12:02:58 -0400

    relations reworked

--- db/migrate/20210719150353_create_breweries.rb
+++ db/migrate/20210719150353_create_breweries.rb
@@ -2,7 +2,8 @@ class CreateBreweries < ActiveRecord::Migration[6.1]
   def change
     create_table :breweries do |t|
       t.string :brewery_name
-      t.string :location
+      t.string :city
+      t.string :state
       t.integer :user_id
 
       t.timestamps
